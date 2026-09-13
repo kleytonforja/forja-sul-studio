a8fd92bc489f29f336435c1ed5615df8789be88a
forjasul <kalvessilvaa@gmail.com>
2026-09-13 19:48:17 -0300

    feat: adiciona menu mobile interativo

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const whatsappButton =
     document.getElementById("whatsappButton");
+const menuToggle = document.getElementById("menuToggle");
+const mainNav = document.getElementById("mainNav");
 
+menuToggle.addEventListener("click", function () {
+
+    mainNav.classList.toggle("active");
+
+    const menuEstaAberto =
+        mainNav.classList.contains("active");
+
+    menuToggle.setAttribute(
+        "aria-expanded",
+        menuEstaAberto
+    );
+
+    menuToggle.textContent =
+        menuEstaAberto ? "✕" : "☰";
+});
 
 whatsappButton.addEventListener(
     "click",
