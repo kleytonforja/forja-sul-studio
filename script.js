b0e4cb34b00e453c3c4e79699e0be1bfa6aefdcc
forjasul <kalvessilvaa@gmail.com>
2026-09-13 10:12:03 -0300

    feat: estrutura inicial do site

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoContato = document.getElementById("botaoContato");
+
+botaoContato.addEventListener("click", function () {
+    alert("Em breve você poderá falar com a Forja Sul Studio pelo WhatsApp!");
+});
